561442beb3d539a73e206309c5ab6daa3138217d
frontendEric852 <93028227+frontendEric852@users.noreply.github.com>
2021-11-01 06:42:35 +0800

    Init todoList

---
+++ src/TodoList.js
@@ -0,0 +1,15 @@
+import React, {Component} from "react";
+
+class TodoList extends Component {
+    render() {
+        return (
+            <div> 
+                <input />
+                <button>Submit</button>
+            </div>
+
+        );
+    }
+}
+
+export default TodoList;
