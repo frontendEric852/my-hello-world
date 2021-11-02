46cee8a6f702ebcb385516e3bda044ca35e1253c
frontendEric852 <93028227+frontendEric852@users.noreply.github.com>
2021-11-03 05:55:14 +0800

    add todoList function

--- src/TodoList.js
+++ src/TodoList.js
@@ -1,13 +1,64 @@
-import React, {Component} from "react";
+import React, { Component } from "react";
 
 class TodoList extends Component {
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            inputValue: '',
+            list: []
+        }
+    }
+
+    handleInputChange(e) {
+        this.setState({
+            inputValue: e.target.value
+        });
+    }
+
+    handleSumbit() {
+        this.setState({
+            list: [...this.state.list, this.state.inputValue],
+            inputValue: ''
+        })
+    }
+
+    handleItemDelete(index) {
+        // imutable
+        const list = [...this.state.list];
+        list.splice(index, 1);
+
+        this.setState({
+            list: list,
+            inputValue: ''
+        });
+    }
+
     render() {
         return (
-            <div> 
-                <input />
-                <button>Submit</button>
+            <div>
+                <div>
+                    <input
+                        value={this.state.inputValue}
+                        onChange={this.handleInputChange.bind(this)}
+                    />
+                    <button onClick={this.handleSumbit.bind(this)}>Submit</button>
+                </div>
+                <ul>
+                    {
+                        this.state.list.map((item, index) => {
+                            return (
+                                <li 
+                                    key={index}
+                                    onClick={this.handleItemDelete.bind(this, index)}
+                                >
+                                    {item}
+                                </li>
+                            )
+                        })
+                    }
+                </ul>
             </div>
-
         );
     }
 }
